d38cba274e869742228f7da89b17c2798bb0785d
Tarun Angalakuduru <tarunangalakuduru07@gmail.com>
2026-01-07 17:06:37 +0530

    updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-statefile--07-01-2026"
-    key    = "vpc/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "ap-south-1"
     encrypt = true
   }

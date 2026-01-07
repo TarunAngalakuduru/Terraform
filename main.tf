34a97d5439946bb4cfc97fb40b0e88f73497d213
Tarun Angalakuduru <tarunangalakuduru07@gmail.com>
2026-01-07 16:42:24 +0530

    Updated terraform.tfvars, main.tf deleted provider.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+  required_version = ">= 1.2"
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+
 resource "aws_vpc" "vpc" {
   cidr_block           = var.vpc_cidr_block
   enable_dns_support   = true
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+aws_region     = "ap-south-1"
+vpc_name       = "demo-vpc"
+vpc_cidr_block = "10.0.0.0/16"
